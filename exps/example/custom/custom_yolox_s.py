8902476c72d2106064dacdd8f9717b474a4f5381
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2024-09-11 02:00:15 +0900

    Create custom_yolox_s.py
    add source

---
+++ exps/example/custom/custom_yolox_s.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 0.33
+        self.width = 0.50
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+        # Define yourself dataset path
+        self.data_dir = "datasets/dog_pose_dataset"
+        self.train_ann = "dog_keypoints_train.json" #/content/YOLOX/datasets/dog_pose_dataset/annotations/dog_keypoints_train.json
+        self.val_ann = "dog_keypoints_val.json"
+
+        self.num_classes = 1
+
+        self.max_epoch = 1
+        self.data_num_workers = 4
+        self.eval_interval = 1
+
+    def get_dataset(self, cache: bool = False, cache_type: str = "ram"):
+        """
+        Get dataset according to cache and cache_type parameters.
+        Args:
+            cache (bool): Whether to cache imgs to ram or disk.
+            cache_type (str, optional): Defaults to "ram".
+                "ram" : Caching imgs to ram for fast training.
+                "disk": Caching imgs to disk for fast training.
+        """
+        from yolox.data import COCODataset, TrainTransform
+
+        return COCODataset(
+            data_dir=self.data_dir,
+            json_file=self.train_ann,
+            name="images/dog_keypoints_train",
+            img_size=self.input_size,
+            preproc=TrainTransform(
+                max_labels=50,
+                flip_prob=self.flip_prob,
+                hsv_prob=self.hsv_prob
+            ),
+            cache=cache,
+            cache_type=cache_type,
+        )
+   
+    def get_eval_dataset(self, **kwargs):
+        from yolox.data import COCODataset, ValTransform
+        testdev = kwargs.get("testdev", False)
+        legacy = kwargs.get("legacy", False)
+
+        return COCODataset(
+            data_dir=self.data_dir,
+            json_file=self.val_ann if not testdev else self.test_ann,
+            name="images/dog_keypoints_val",
+            img_size=self.test_size,
+            preproc=ValTransform(legacy=legacy),
+        )
